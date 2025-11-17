154cb13dbf6d24d396ec921acc4430878ed39874
prernagrg <grgprena2023@gmail.com>
2025-11-17 09:26:21 +0545

    added package1 and package2 and import package1 to 1 to display

---
+++ package1/student.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class student {
+    private int a = 10;
+    protected int b = 20;
+
+    public void display() {
+
+        System.out.println("I am in student class");
+    }
+}
---
+++ package2/admin.java
@@ -0,0 +1,11 @@
+package package2;
+
+import package1.student;
+
+public class admin {
+
+    public static void main(String[] args) {
+        student s = new student();
+        s.display();
+    }
+}
