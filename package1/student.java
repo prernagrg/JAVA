283561988854cdf62158885a3e36afef5d6384d9
prernagrg <grgprena2023@gmail.com>
2025-11-18 09:42:12 +0545

    accessing protected using extends

--- package1/student.java
+++ package1/student.java
@@ -3,9 +3,15 @@ package package1;
 public class student {
     private int a = 10;
     protected int b = 20;
+    public int c = 30;
+    int d = 40;
 
     public void display() {
 
         System.out.println("I am in student class");
+        System.out.println("accessing private a =" + a);
+        System.out.println("accessing protected b =" + b);
+        System.out.println("accessing public c  =" + c);
+        System.out.println("default = " + d);
     }
 }
--- package2/admin.java
+++ package2/admin.java
@@ -2,10 +2,19 @@ package package2;
 
 import package1.student;
 
-public class admin {
+public class admin extends student {
+
+    public void show() {
+        System.out.println("Accessing protected b = " + b);
+    }
 
     public static void main(String[] args) {
         student s = new student();
         s.display();
+        // System.out.println("accessing protected b = " + s.b);
+        System.out.println("accessing public c = " + s.c);
+        admin a = new admin();
+        a.show();
     }
+
 }
