37e83e7f2e6ae12423fab72f800a5e694a24c784
prernagrg <grgprena2023@gmail.com>
2025-11-23 22:43:53 +0545

    added teacher

--- package1/student.java
+++ package1/student.java
@@ -4,7 +4,7 @@ public class student {
     private int a = 10;
     protected int b = 20;
     public int c = 30;
-    int d = 40;
+    int d = 40; // default -> accessible in same package
 
     public void display() {
 
---
+++ package1/teacher.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class teacher {
+    public void testPackage() {
+        student s = new student();
+        int d = 10;
+        System.out.println("default d = " + d);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
